57f2ea128cdbecedac7ec5952ed33d627bcd029f
Celeste Senoseain <csenoseain@makingsense.com>
2015-04-21 16:28:43 -0300

    About page is empty #64

--- public/scripts/controllers/aboutCtrl.js
+++ public/scripts/controllers/aboutCtrl.js
@@ -1,5 +1,6 @@
 angular
     .module('app')
     .controller('aboutCtrl', function($scope) {
-        $scope.message = 'The page is about.."';
+        $scope.message = "We started this project with the goal to serve Making Sense's designers teams with a useful repository while learning technologies such as AngularJS, MongoDB, NodeJS and ExpressJS. Said this, images, photos, illustrations and vectors will be supported in the first version of MS-Images.";
+        $scope.authors = "Rosario Mensi & Celeste Senoseain @2015"
     });
