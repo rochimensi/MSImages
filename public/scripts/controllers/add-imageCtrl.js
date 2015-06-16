33edca94ea8b0ff15fd023887667399d80140061
Celeste Senoseain <csenoseain@makingsense.com>
2015-06-16 16:14:33 -0300

    Feature: Validations. Required field into add image

--- public/scripts/controllers/add-imageCtrl.js
+++ public/scripts/controllers/add-imageCtrl.js
@@ -3,6 +3,7 @@ app.controller('add-imageCtrl',['$scope','$location','imageService','fileReader'
 
         $scope.save = function () {
             var tags;
+            $scope.submitted = true;
             if($scope.name && $scope.file && $scope.tags) {
                imageService.create($scope.name, $scope.description, $scope.contributorSelected, $scope.tags, $scope.file, $scope.albumSelected)
                    .success(function (data, status, headers, config) {
@@ -19,16 +20,18 @@ app.controller('add-imageCtrl',['$scope','$location','imageService','fileReader'
             $location.path("/images");
         };
 
-        $scope.getFile = function () {
+        $scope.getFile = function (file) {
             $scope.progress = 0;
             fileReader.readAsDataUrl($scope.file, $scope)
                 .then(function(result) {
                     $scope.imageSrc = result;
+                    $scope.imageName = $scope.file.name;
                 });
         };
 
         $scope.init = function () {
             $scope.titleView = "Upload new image";
+            $scope.submitted = false;
             $scope.contributorSelected = undefined;
             $scope.albumSelected = undefined;
             $scope.size = false;
