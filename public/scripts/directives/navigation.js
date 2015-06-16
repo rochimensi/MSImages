1d4edcddd4a4bd738f86bc541c05748cecf82754
csenoseain <csenoseain>
2015-06-16 15:09:53 -0300

    Feature: Dinamic Menu with directive

---
+++ public/scripts/directives/navigation.js
@@ -0,0 +1,12 @@
+app.directive('navigation', function() {
+    return {
+            restrict: 'E',
+        // If we want to use items inside the directive
+        // we use items
+        // out to the directive use =itemMenu or "=" to use the same name
+           scope: {
+                items: '=itemMenu'
+            },
+        templateUrl : '../partials/nav.html'
+    };
+});
