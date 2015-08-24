31ae78fb4ee66e1f9ba536786a0248df900bdeed
csenoseain <csenoseain>
2015-08-24 17:04:13 -0300

    remove irrelevant things

--- public/scripts/directives/navigation.js
+++ public/scripts/directives/navigation.js
@@ -4,9 +4,7 @@ app.directive('navigation', function() {
         // If we want to use items inside the directive
         // we use items
         // out to the directive use =itemMenu or "=" to use the same name
-           scope: {
-                items: '=itemMenu'
-            },
+
         templateUrl : '../partials/nav.html'
     };
 });
